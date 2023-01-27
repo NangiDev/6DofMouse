5071618c16e28e4202c9314ed77a5d2f8d9c2f62
Joel Setterberg <j.settrbrg@gmail.com>
2023-01-27 14:59:54 +0100

    Setting up for reading from analog inputs

--- head/src/main.cpp
+++ head/src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 
 #define DEBUG
+#define DOF 6
+
+union Ints
+{
+  int values[2];
+  long combined;
+} myInts;
 
 void setup()
 {
@@ -12,13 +19,12 @@ void setup()
   Serial1.write(1);
 }
 
-union Ints
-{
-  int values[2];
-  long combined;
-} myInts;
-
 int tailX, tailY;
+int coords[DOF] = {512, 512, 512, 512, 512, 512};
+
+void calculate()
+{
+}
 
 void loop()
 {
@@ -27,19 +33,42 @@ void loop()
     Serial1.write(0);
     Serial1.readBytes((byte *)&myInts.combined, sizeof(myInts.combined));
 
-    int tailX = myInts.values[0];
-    int tailY = myInts.values[1];
+    coords[0] = analogRead(A0);
+    coords[1] = analogRead(A2);
+    coords[2] = myInts.values[0];
+    coords[3] = analogRead(A1);
+    coords[4] = analogRead(A3);
+    coords[5] = myInts.values[1];
 
 #if defined(DEBUG)
-    Serial.print("\tX: ");
-    Serial.println(tailX);
-    Serial.print("\tY: ");
-    Serial.println(tailY);
+    coords[0] = random(0, 1024);
+    coords[3] = random(0, 1024);
+    coords[1] = random(0, 1024);
+    coords[4] = random(0, 1024);
+
+    Serial.println("\tJoystick 0");
+    Serial.print("\t\tX: ");
+    Serial.println(coords[0]);
+    Serial.print("\t\tY: ");
+    Serial.println(coords[3]);
+    Serial.println();
+    Serial.println("\tJoystick 1");
+    Serial.print("\t\tX: ");
+    Serial.println(coords[1]);
+    Serial.print("\t\tY: ");
+    Serial.println(coords[4]);
+    Serial.println();
+    Serial.println("\tJoystick 2");
+    Serial.print("\t\tX: ");
+    Serial.println(coords[2]);
+    Serial.print("\t\tY: ");
+    Serial.println(coords[5]);
     Serial.println();
     Serial.println("== Asking for more coordiates ==");
     delay(1000);
 #endif // DEBUG
 
     Serial1.write(1);
+    calculate();
   }
 }
